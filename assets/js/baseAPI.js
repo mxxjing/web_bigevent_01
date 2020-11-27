f82544e8a9d8d696840e1721e31521fbe2262072
mxxjing <949672675@qq.com>
2020-11-27 15:57:49 +0800

    个人中心功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -26,10 +26,10 @@ $.ajaxPrefilter(function (option) {
         console.log(res);
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status !== 0 && res.responseJSON.message === '身份认证失败！') {
-            // 强制清空token
-            localStorage.removeItem('token')
-            // 强制跳转到登录页面
-            location.href = '/login.html'
+            // // 强制清空token
+            // localStorage.removeItem('token')
+            // // 强制跳转到登录页面
+            // location.href = '/login.html'
         }
     }
 }) 
